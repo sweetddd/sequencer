2a7fd1cff5b1ee75f0c37b1f8561514809aca036
sunny.shi <sunny.shi@iswallet.me>
2023-05-27 08:21:41 +0800

    fix single node script and add multi-node script

--- local_node.sh
+++ local_node.sh
@@ -11,8 +11,8 @@ MONIKER="localtestnet"
 KEYRING="test"
 KEYALGO="eth_secp256k1"
 LOGLEVEL="info"
-# Set dedicated home directory for the evmosd instance
-HOMEDIR="$HOME/.tmp-evmosd"
+# Set dedicated home directory for the sequencerd instance
+HOMEDIR="$HOME/.tmp-sequencerd"
 # to trace evm
 #TRACE="--trace"
 TRACE=""
@@ -51,16 +51,16 @@ if [[ $overwrite == "y" || $overwrite == "Y" ]]; then
 	rm -rf "$HOMEDIR"
 
 	# Set client config
-	evmosd config keyring-backend $KEYRING --home "$HOMEDIR"
-	evmosd config chain-id $CHAINID --home "$HOMEDIR"
+	sequencerd config keyring-backend $KEYRING --home "$HOMEDIR"
+	sequencerd config chain-id $CHAINID --home "$HOMEDIR"
 
 	# If keys exist they should be deleted
 	for KEY in "${KEYS[@]}"; do
-		evmosd keys add "$KEY" --keyring-backend $KEYRING --algo $KEYALGO --home "$HOMEDIR"
+		sequencerd keys add "$KEY" --keyring-backend $KEYRING --algo $KEYALGO --home "$HOMEDIR"
 	done
 
 	# Set moniker and chain-id for Evmos (Moniker can be anything, chain-id must be an integer)
-	evmosd init $MONIKER -o --chain-id $CHAINID --home "$HOMEDIR"
+	sequencerd init $MONIKER -o --chain-id $CHAINID --home "$HOMEDIR"
 
 	# Change parameter token denominations to aevmos
 	jq '.app_state["staking"]["params"]["bond_denom"]="aevmos"' "$GENESIS" >"$TMP_GENESIS" && mv "$TMP_GENESIS" "$GENESIS"
@@ -72,6 +72,10 @@ if [[ $overwrite == "y" || $overwrite == "Y" ]]; then
 	# Set gas limit in genesis
 	jq '.consensus_params["block"]["max_gas"]="10000000"' "$GENESIS" >"$TMP_GENESIS" && mv "$TMP_GENESIS" "$GENESIS"
 
+	# Set block time
+#  jq '.consensus_params["block"]["time_iota_ms"]="16"' "$GENESIS" >"$TMP_GENESIS" && mv "$TMP_GENESIS" "$GENESIS"
+
+
 	# Set claims start time
 	current_date=$(date -u +"%Y-%m-%dT%TZ")
 	jq -r --arg current_date "$current_date" '.app_state["claims"]["params"]["airdrop_start_time"]=$current_date' "$GENESIS" >"$TMP_GENESIS" && mv "$TMP_GENESIS" "$GENESIS"
@@ -79,7 +83,7 @@ if [[ $overwrite == "y" || $overwrite == "Y" ]]; then
 	# Set claims records for validator account
 	amount_to_claim=10000
 	claims_key=${KEYS[0]}
-	node_address=$(evmosd keys show "$claims_key" --keyring-backend $KEYRING --home "$HOMEDIR" | grep "address" | cut -c12-)
+	node_address=$(sequencerd keys show "$claims_key" --keyring-backend $KEYRING --home "$HOMEDIR" | grep "address" | cut -c12-)
 	jq -r --arg node_address "$node_address" --arg amount_to_claim "$amount_to_claim" '.app_state["claims"]["claims_records"]=[{"initial_claimable_amount":$amount_to_claim, "actions_completed":[false, false, false, false],"address":$node_address}]' "$GENESIS" >"$TMP_GENESIS" && mv "$TMP_GENESIS" "$GENESIS"
 
 	# Set claims decay
@@ -132,9 +136,15 @@ if [[ $overwrite == "y" || $overwrite == "Y" ]]; then
 	sed -i.bak 's/pruning-keep-recent = "0"/pruning-keep-recent = "2"/g' "$APP_TOML"
 	sed -i.bak 's/pruning-interval = "0"/pruning-interval = "10"/g' "$APP_TOML"
 
+  # disable produce empty block
+#  sed -i '' 's/create_empty_blocks = true/create_empty_blocks = false/g' "$CONFIG"
+#  sed -i '' 's/create_empty_blocks_interval = "0s"/create_empty_blocks_interval = "30s"/g' "$CONFIG"
+
+
+
 	# Allocate genesis accounts (cosmos formatted addresses)
 	for KEY in "${KEYS[@]}"; do
-		evmosd add-genesis-account "$KEY" 100000000000000000000000000aevmos --keyring-backend $KEYRING --home "$HOMEDIR"
+		sequencerd add-genesis-account "$KEY" 100000000000000000000000000aevmos --keyring-backend $KEYRING --home "$HOMEDIR"
 	done
 
 	# bc is required to add these big numbers
@@ -142,19 +152,19 @@ if [[ $overwrite == "y" || $overwrite == "Y" ]]; then
 	jq -r --arg total_supply "$total_supply" '.app_state["bank"]["supply"][0]["amount"]=$total_supply' "$GENESIS" >"$TMP_GENESIS" && mv "$TMP_GENESIS" "$GENESIS"
 
 	# Sign genesis transaction
-	evmosd gentx "${KEYS[0]}" 1000000000000000000000aevmos --keyring-backend $KEYRING --chain-id $CHAINID --home "$HOMEDIR"
+	sequencerd gentx "${KEYS[0]}" 1000000000000000000000aevmos --keyring-backend $KEYRING --chain-id $CHAINID --home "$HOMEDIR"
 	## In case you want to create multiple validators at genesis
-	## 1. Back to `evmosd keys add` step, init more keys
-	## 2. Back to `evmosd add-genesis-account` step, add balance for those
-	## 3. Clone this ~/.evmosd home directory into some others, let's say `~/.clonedEvmosd`
+	## 1. Back to `sequencerd keys add` step, init more keys
+	## 2. Back to `sequencerd add-genesis-account` step, add balance for those
+	## 3. Clone this ~/.sequencerd home directory into some others, let's say `~/.clonedEvmosd`
 	## 4. Run `gentx` in each of those folders
-	## 5. Copy the `gentx-*` folders under `~/.clonedEvmosd/config/gentx/` folders into the original `~/.evmosd/config/gentx`
+	## 5. Copy the `gentx-*` folders under `~/.clonedEvmosd/config/gentx/` folders into the original `~/.sequencerd/config/gentx`
 
 	# Collect genesis tx
-	evmosd collect-gentxs --home "$HOMEDIR"
+	sequencerd collect-gentxs --home "$HOMEDIR"
 
 	# Run this to ensure everything worked and that the genesis file is setup correctly
-	evmosd validate-genesis --home "$HOMEDIR"
+	sequencerd validate-genesis --home "$HOMEDIR"
 
 	if [[ $1 == "pending" ]]; then
 		echo "pending mode is on, please wait for the first block committed."
@@ -162,4 +172,4 @@ if [[ $overwrite == "y" || $overwrite == "Y" ]]; then
 fi
 
 # Start the node (remove the --pruning=nothing flag if historical queries are not needed)
-evmosd start --metrics "$TRACE" --log_level $LOGLEVEL --minimum-gas-prices=0.0001aevmos --json-rpc.api eth,txpool,personal,net,debug,web3 --api.enable --home "$HOMEDIR"
+sequencerd start --metrics "$TRACE" --log_level $LOGLEVEL --minimum-gas-prices=0.0001aevmos --json-rpc.api eth,txpool,personal,net,debug,web3 --api.enable --home "$HOMEDIR"
---
+++ multi-node.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+rm -rf data/node0
+rm -rf data/node1
+rm -rf data/node2
+
+sequencerd init node0 --chain-id testnet_9000-1 --home data/node0
+sequencerd init node1 --chain-id testnet_9000-1 --home data/node1
+sequencerd init node2 --chain-id testnet_9000-1 --home data/node2
+
+sequencerd keys add dev0
+sequencerd keys add dev1
+sequencerd keys add dev2
+
+# Change parameter token denominations to aseq
+cat data/node0/config/genesis.json | jq '.app_state["evm"]["params"]["evm_denom"]="aseq"' > data/node0/config/tmp_genesis.json && mv data/node0/config/tmp_genesis.json data/node0/config/genesis.json
+cat data/node0/config/genesis.json | jq '.app_state["staking"]["params"]["bond_denom"]="aseq"' > data/node0/config/tmp_genesis.json && mv data/node0/config/tmp_genesis.json data/node0/config/genesis.json
+cat data/node0/config/genesis.json | jq '.app_state["crisis"]["constant_fee"]["denom"]="aseq"' > data/node0/config/tmp_genesis.json && mv data/node0/config/tmp_genesis.json data/node0/config/genesis.json
+cat data/node0/config/genesis.json | jq '.app_state["gov"]["deposit_params"]["min_deposit"][0]["denom"]="aseq"' > data/node0/config/tmp_genesis.json && mv data/node0/config/tmp_genesis.json data/node0/config/genesis.json
+cat data/node0/config/genesis.json | jq '.app_state["inflation"]["params"]["mint_denom"]="aseq"' > data/node0/config/tmp_genesis.json && mv data/node0/config/tmp_genesis.json data/node0/config/genesis.json
+
+cat data/node1/config/genesis.json | jq '.app_state["evm"]["params"]["evm_denom"]="aseq"' > data/node1/config/tmp_genesis.json && mv data/node1/config/tmp_genesis.json data/node1/config/genesis.json
+cat data/node1/config/genesis.json | jq '.app_state["staking"]["params"]["bond_denom"]="aseq"' > data/node1/config/tmp_genesis.json && mv data/node1/config/tmp_genesis.json data/node1/config/genesis.json
+cat data/node1/config/genesis.json | jq '.app_state["crisis"]["constant_fee"]["denom"]="aseq"' > data/node1/config/tmp_genesis.json && mv data/node1/config/tmp_genesis.json data/node1/config/genesis.json
+cat data/node1/config/genesis.json | jq '.app_state["gov"]["deposit_params"]["min_deposit"][0]["denom"]="aseq"' > data/node1/config/tmp_genesis.json && mv data/node1/config/tmp_genesis.json data/node1/config/genesis.json
+cat data/node1/config/genesis.json | jq '.app_state["inflation"]["params"]["mint_denom"]="aseq"' > data/node1/config/tmp_genesis.json && mv data/node1/config/tmp_genesis.json data/node1/config/genesis.json
+
+cat data/node2/config/genesis.json | jq '.app_state["evm"]["params"]["evm_denom"]="aseq"' > data/node2/config/tmp_genesis.json && mv data/node2/config/tmp_genesis.json data/node2/config/genesis.json
+cat data/node2/config/genesis.json | jq '.app_state["staking"]["params"]["bond_denom"]="aseq"' > data/node2/config/tmp_genesis.json && mv data/node2/config/tmp_genesis.json data/node2/config/genesis.json
+cat data/node2/config/genesis.json | jq '.app_state["crisis"]["constant_fee"]["denom"]="aseq"' > data/node2/config/tmp_genesis.json && mv data/node2/config/tmp_genesis.json data/node2/config/genesis.json
+cat data/node2/config/genesis.json | jq '.app_state["gov"]["deposit_params"]["min_deposit"][0]["denom"]="aseq"' > data/node2/config/tmp_genesis.json && mv data/node2/config/tmp_genesis.json data/node2/config/genesis.json
+cat data/node2/config/genesis.json | jq '.app_state["mint"]["params"]["mint_denom"]="aseq"' > data/node2/config/tmp_genesis.json && mv data/node2/config/tmp_genesis.json data/node2/config/genesis.json
+
+
+
+#sequencerd add-genesis-account evmos1muejcvu6m8ntsl3a4lmjukg0wdlm8ewd6d5res 10000000000000000000000000000aseq --home data/node0
+#sequencerd add-genesis-account evmos127sdadcyzemwa0rtdde65hqksnmwmmrp0dkgcl 10000000000000000000000000000aseq --home data/node0
+#sequencerd add-genesis-account evmos1x9kfp967y749w34tjrdkgvszyr296e5e0r6e73 100000000000000000000000000aseq --home data/node0
+
+
+sequencerd add-genesis-account dev0 100000000000000000000aseq --home data/node0
+sequencerd add-genesis-account dev1 100000000000000000000aseq --home data/node1
+sequencerd add-genesis-account dev2 100000000000000000000aseq --home data/node2
+
+
+sequencerd gentx  dev0 100000000000000000000aseq --home data/node0  --node-id $(sequencerd tendermint show-node-id --home data/node0) --chain-id testnet_9000-1
+sequencerd gentx  dev1  100000000000000000000aseq --home data/node1  --node-id $(sequencerd tendermint show-node-id --home data/node1) --chain-id testnet_9000-1
+sequencerd gentx  dev2 100000000000000000000aseq --home data/node2  --node-id $(sequencerd tendermint show-node-id --home data/node2) --chain-id testnet_9000-1
+
+
+
+sequencerd collect-gentxs --home data/node0
+sequencerd collect-gentxs --home data/node1
+sequencerd collect-gentxs --home data/node2
+
+
+#TRACE=""
+#
+## Start the node (remove the --pruning=nothing flag if historical queries are not needed)
+sequencerd start --metrics "$TRACE" --log_level="info" --minimum-gas-prices=0.0001aseq --json-rpc.api eth,txpool,personal,net,debug,web3 --api.enable --home data/node0
+sequencerd start --metrics "$TRACE" --log_level="info" --minimum-gas-prices=0.0001aseq --json-rpc.api eth,txpool,personal,net,debug,web3 --api.enable --home data/node1
+sequencerd start --metrics "$TRACE" --log_level="info" --minimum-gas-prices=0.0001aseq --json-rpc.api eth,txpool,personal,net,debug,web3 --api.enable --home data/node2
