18f2bff32d035a794b54f53c3ff922dfdb22a99c
henry <henry@everylink.ai>
2023-06-10 15:04:18 +0800

    添加testnet 脚本

--- multi-node-testnet.sh
+++ multi-node-testnet.sh
@@ -8,9 +8,9 @@ sequencerd init node0 --chain-id testnet_8999-1 --home data/testnet/zkevm-sequen
 sequencerd init node1 --chain-id testnet_8999-1 --home data/testnet/zkevm-sequencer-1
 sequencerd init node2 --chain-id testnet_8999-1 --home data/testnet/zkevm-sequencer-2
 
-sequencerd keys add testnet0  --home data/testnet/zkevm-sequencer-0
-sequencerd keys add testnet1  --home data/testnet/zkevm-sequencer-1
-sequencerd keys add testnet2  --home data/testnet/zkevm-sequencer-2
+yes 12345678 | sequencerd keys add testnet0  --home data/testnet/zkevm-sequencer-0
+yes 12345678 | sequencerd keys add testnet1  --home data/testnet/zkevm-sequencer-1
+yes 12345678 | sequencerd keys add testnet2  --home data/testnet/zkevm-sequencer-2
 
 jq '.app_state["staking"]["params"]["bond_denom"]="aseq"' data/testnet/zkevm-sequencer-0/config/genesis.json >data/testnet/zkevm-sequencer-0/config/tmp_genesis.json && mv data/testnet/zkevm-sequencer-0/config/tmp_genesis.json data/testnet/zkevm-sequencer-0/config/genesis.json
 jq '.app_state["crisis"]["constant_fee"]["denom"]="aseq"' data/testnet/zkevm-sequencer-0/config/genesis.json >data/testnet/zkevm-sequencer-0/config/tmp_genesis.json && mv data/testnet/zkevm-sequencer-0/config/tmp_genesis.json data/testnet/zkevm-sequencer-0/config/genesis.json
@@ -19,16 +19,16 @@ jq '.app_state["evm"]["params"]["evm_denom"]="aseq"' data/testnet/zkevm-sequence
 jq '.app_state["inflation"]["params"]["mint_denom"]="aseq"' data/testnet/zkevm-sequencer-0/config/genesis.json >data/testnet/zkevm-sequencer-0/config/tmp_genesis.json && mv data/testnet/zkevm-sequencer-0/config/tmp_genesis.json data/testnet/zkevm-sequencer-0/config/genesis.json
 
 
-sequencerd add-genesis-account testnet0 100000000000000000000000000aseq --keyring-backend os --home data/testnet/zkevm-sequencer-0
-sequencerd add-genesis-account testnet1 100000000000000000000000000aseq --keyring-backend os --home data/testnet/zkevm-sequencer-0
-sequencerd add-genesis-account testnet2 100000000000000000000000000aseq --keyring-backend os --home data/testnet/zkevm-sequencer-0
-sequencerd add-genesis-account testnet1 100000000000000000000000000aseq --keyring-backend os --home data/testnet/zkevm-sequencer-1
-sequencerd add-genesis-account testnet2 100000000000000000000000000aseq --keyring-backend os --home data/testnet/zkevm-sequencer-2
+yes 12345678 | sequencerd add-genesis-account testnet0 100000000000000000000000000aseq --keyring-backend os --home data/testnet/zkevm-sequencer-0
+yes 12345678 | sequencerd add-genesis-account testnet1 100000000000000000000000000aseq --keyring-backend os --home data/testnet/zkevm-sequencer-0
+yes 12345678 | sequencerd add-genesis-account testnet2 100000000000000000000000000aseq --keyring-backend os --home data/testnet/zkevm-sequencer-0
+yes 12345678 | sequencerd add-genesis-account testnet1 100000000000000000000000000aseq --keyring-backend os --home data/testnet/zkevm-sequencer-1
+yes 12345678 | sequencerd add-genesis-account testnet2 100000000000000000000000000aseq --keyring-backend os --home data/testnet/zkevm-sequencer-2
 
 
-sequencerd gentx  testnet0 1000000000000000000000aseq --home data/testnet/zkevm-sequencer-0  --keyring-backend os --chain-id testnet_9000-1
-sequencerd gentx  testnet1 1000000000000000000000aseq --home data/testnet/zkevm-sequencer-1  --keyring-backend os --chain-id testnet_9000-1
-sequencerd gentx  testnet2 1000000000000000000000aseq --home data/testnet/zkevm-sequencer-2  --keyring-backend os --chain-id testnet_9000-1
+yes 12345678 | sequencerd gentx  testnet0 1000000000000000000000aseq --home data/testnet/zkevm-sequencer-0  --keyring-backend os --chain-id testnet_9000-1
+yes 12345678 | sequencerd gentx  testnet1 1000000000000000000000aseq --home data/testnet/zkevm-sequencer-1  --keyring-backend os --chain-id testnet_9000-1
+yes 12345678 | sequencerd gentx  testnet2 1000000000000000000000aseq --home data/testnet/zkevm-sequencer-2  --keyring-backend os --chain-id testnet_9000-1
 
 
 
